af71257a22b89adc1e299220bcde4d6f9dc012bb
nneveu <nneveu@hawk.iit.edu>
2019-01-28 15:56:29 -0800

    updating bsub script

--- slac/bsub.sh
+++ slac/bsub.sh
@@ -1,28 +1,28 @@
 #!/bin/bash 
 
-#BSUB -q bubble ####beamphysics-mpi
+#BSUB -q beamphys-centos7
 #BSUB -J regtest
 #BSUB -W 10:00
 #BSUB -n 8
 #BSUB -o regtest.out
 #BSUB -e regtest.err
+#BSUB -R "span[ptile=36]"
+#BSUB -x 
+#####BSUB -sla bpmp #for bullet only 
 
-export OPAL_EXE=/nfs/slac/g/beamphysics/nneveu/opal_test/software/bin
+export OPAL_EXE=/gpfs/slac/staas/fs1/g/g.beamphysics/neveu/opal_test/software/bin
+export PATH=/gpfs/slac/staas/fs1/g/g.beamphysics/neveu/opal_test/software/H5hut-2.0.0rc4_build/lib:$PATH
+export LD_LIBRARY_PATH=/gpfs/slac/staas/fs1/g/g.beamphysics/neveu/opal_test/software/H5hut-2.0.0rc4_build/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/gpfs/slac/staas/fs1/g/g.beamphysics/neveu/opal_test/software/H5hut-2.0.0rc4_build/lib:$LIBRARY_PATH
 
 #Test
 echo -e "Bsub job ID: $LSB_JOBID"
 echo -e "Working dir: $LS_SUBCWD"
-echo -e "Cd to scratch..."
-#cd $PBS_O_WORKDIR
-cp *.in /scratch
-cd /scratch
 
 # A little useful information for the log file...
 echo -e "Master process running on: $HOSTNAME"
 echo -e "Directory is:  $PWD"
-
 mpirun -n 8 $OPAL_EXE/opal regtest-v2.0.in
-cp *.stat $LS_SUBCWD
 
 # Print the date again -- when finished
 echo Finished at: `date`
